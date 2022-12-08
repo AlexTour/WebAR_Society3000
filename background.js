ab4a9761b435dbded61a6281e565b4d8650888cd
Alexandros Touramanis <32987136+AlexTour@users.noreply.github.com>
2022-12-08 15:50:03 +0100

    Update background.js

--- background.js
+++ background.js
@@ -6,11 +6,11 @@ var c = document.getElementById("c");
         c.width = window.innerWidth;
 
         //chinese characters - taken from the unicode charset
-        var matrix = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%+-/~{[|`]}";
+        var matrix = "SOCIETY3000";//"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%+-/~{[|`]}";
         //converting the string into an array of single characters
         matrix = matrix.split("");
 
-        var font_size = 10;
+        var font_size = 16; // 10
         var columns = c.width/font_size; //number of columns for the rain
         //an array of drops - one per column
         var drops = [];
@@ -24,7 +24,7 @@ var c = document.getElementById("c");
         {
             //Black BG for the canvas
             //translucent BG to show trail
-            ctx.fillStyle = "rgba(61,61,63, 0.08)";
+            ctx.fillStyle = "rgba(0,0,0, 0.05)";
             ctx.fillRect(0, 0, c.width, c.height);
 
             ctx.fillStyle = "#00FF00";//green text
@@ -39,12 +39,12 @@ var c = document.getElementById("c");
 
                 //sending the drop back to the top randomly after it has crossed the screen
                 //adding a randomness to the reset to make the drops scattered on the Y axis
-                if(drops[i]*font_size > c.height && Math.random() > 0.975)
-                    drops[i] = 0;
+               // if(drops[i]*font_size > c.height && Math.random() > 0.975)  //this is where it becomes matrix
+                //    drops[i] = 0;
 
                 //incrementing Y coordinate
                 drops[i]++;
             }
         }
 
-        setInterval(draw, 70);
+        setInterval(draw, 50);
